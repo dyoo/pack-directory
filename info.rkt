251fe73c37c16f3c09bc3ba85a03e35f0b939792
Danny Yoo <dyoo@cs.wpi.edu>
2010-10-18 22:16:37 -0400

    adding minimal info.rkt

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang setup/infotab
+(define name "pack-directory")
+(define blurb
+  '("Packs the current directory into bytes, and visa-versa."))
+(define primary-file "pack-directory.rkt")
+(define categories '(system))
