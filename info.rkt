3cb437f7c4288c9d28fc79d24bfe4855437110e4
Danny Yoo <dyoo@cs.wpi.edu>
2010-10-18 22:19:50 -0400

    updating info.rkt with a little more information.

--- info.rkt
+++ info.rkt
@@ -2,5 +2,9 @@
 (define name "pack-directory")
 (define blurb
   '("Packs the current directory into bytes, and visa-versa."))
+(define categories '(io))
+(define repositories '("4.x"))
+(define can-be-loaded-with 'all)
 (define primary-file "pack-directory.rkt")
-(define categories '(system))
+(define required-core-version "5.0.1")
+(define version "1.0")
